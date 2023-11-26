5585257c032997afafae0a5926e9d056346287df
Konstantinas Gucul <guculiux@gmail.com>
2023-11-26 16:12:59 +0000

    complete JS

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("https://icanhazdadjoke.com", config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    });
+}
